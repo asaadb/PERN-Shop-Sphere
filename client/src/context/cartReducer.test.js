694438f239daf50694029eb0bd65ac177f8687ab
Asaad Budayr <budayrasaa@AD.ECS.VUW.AC.NZ>
2026-01-06 19:57:05 +1300

    test: Add cart reducer unit test for clearing the cart

--- client/src/context/cartReducer.test.js
+++ client/src/context/cartReducer.test.js
@@ -30,15 +30,28 @@ it('should update the quantity of an existing item', () => {
 it('should remove an item from the cart', () => {
   const initialState = [
     { id: 1, product_id: 1, name: 'Test Product', quantity: 2 },
-    { id: 2, product_id: 2, name: 'Another Product', quantity: 1 }
+    { id: 2, product_id: 2, name: 'Another Product', quantity: 1 },
   ];
   const action = {
     type: 'REMOVE_ITEM',
-    payload: 1 // id of the item to remove
+    payload: 1, // id of the item to remove
   };
   const newState = cartReducer(initialState, action);
 
   expect(newState).toHaveLength(1);
   expect(newState[0].id).toBe(2);
   expect(newState[0].name).toBe('Another Product');
+});
+
+it('should clear all items from the cart', () => {
+  const initialState = [
+    { id: 1, product_id: 1, name: 'Test Product', quantity: 2 },
+    { id: 2, product_id: 2, name: 'Another Product', quantity: 1 },
+  ];
+  const action = {
+    type: 'CLEAR_CART',
+  };
+  const newState = cartReducer(initialState, action);
+
+  expect(newState).toHaveLength(0);
 });
