e550aebb4fa75be85cc0877379664710dcb88c4e
Asaad Budayr <budayrasaa@AD.ECS.VUW.AC.NZ>
2026-01-10 20:10:48 +1300

    chore: update seed.sql with  product data and Unsplash image URLs

--- database/seed.sql
+++ database/seed.sql
@@ -1,12 +1,12 @@
 -- Insert categories
 INSERT INTO categories (name, description) VALUES
 ('Electronics', 'Electronic devices and accessories'),
-('Clothing', 'Apparel and fashion items'),
+('Clothing', ' fashion items'),
 ('Books', 'Books and publications');
 
 -- Insert products
 INSERT INTO products (name, description, price, category_id, stock_quantity, image_url) VALUES
-('iPhone 15 Pro', 'Latest Apple smartphone with A17 chip', 999.99, 1, 50, '/images/iphone15.jpg'),
-('Samsung Galaxy S24', 'Flagship Android phone', 899.99, 1, 30, '/images/galaxy-s24.jpg'),
-('Cotton T-Shirt', 'Comfortable cotton t-shirt', 19.99, 2, 100, '/images/tshirt.jpg'),
-('JavaScript Book', 'Learn JavaScript programming', 39.99, 3, 25, '/images/js-book.jpg');
+('iPhone 15 Pro', 'Latest Apple smartphone', 999.99, 1, 50, 'https://plus.unsplash.com/premium_photo-1668612078695-48b09fd23398?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('Samsung Galaxy S24', 'Flagship Android phone', 899.89, 1, 30, 'https://plus.unsplash.com/premium_photo-1668612078695-48b09fd23398?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('Cotton T-Shirt', 'Comfortable cotton t-shirt', 19.9, 2, 100, 'https://plus.unsplash.com/premium_photo-1668612078695-48b09fd23398?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+('JavaScript Book', 'learn JavaScript programming', 100, 3, 25, 'https://plus.unsplash.com/premium_photo-1668612078695-48b09fd23398?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
